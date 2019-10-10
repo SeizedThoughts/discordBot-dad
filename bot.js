780c208977e5fba0a6cd444a0d910604fb924f21
SeizedThoughts <43914242+SeizedThoughts@users.noreply.github.com>
2019-10-09 19:06:02 -0700

    swear integration

--- bot.js
+++ bot.js
@@ -3,6 +3,9 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 const info = JSON.parse(fs.readFileSync('./token.JSON'))
 const token = info.token
+const swears =  JSON.parse(fs.readFileSync('./swears.JSON'))
+
+console.log(swears["ass"])
 
 client.once('ready', () => {
 	console.log('Ready!')
@@ -12,8 +15,10 @@ client.on('message', msg => {
     var prefix = msg.content.split(' ', 2)[0].toLowerCase()
 
     if(prefix === "i'm" || prefix === "i’m" || prefix === "im"){
-        var name = msg.content.slice(prefix.length + 1).replace(/\b\w/g, char => char.toUpperCase())
-        msg.channel.send(`Hi ${name}, I'm Dad.`)
+        //.replace(/\b\w/g, char => char.toUpperCase())
+        var name = msg.content.slice(prefix.length + 1).toLowerCase()
+        var cName = name.split(' ').map(part => (swears[part] === undefined ? part : swears[part])).join(' ').replace(/\b\w/g, char => char.toUpperCase())
+        msg.channel.send(`Hi ${cName}, I'm Dad.`)
     }
 
 });
